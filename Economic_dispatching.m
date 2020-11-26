1b5721023b1db80b78c0a67bc0f4dcd21697aed2
Haider Ali <49084945+haider2303@users.noreply.github.com>
2020-11-26 11:41:12 +0100

    Update Economic_dispatching.m

--- Economic_dispatching.m
+++ Economic_dispatching.m
@@ -1,3 +1,8 @@
+%Author: Haider Ali
+%Code: This is economic dispatch code for three power plants.
+%Explanation: The equation for each power plant is quadratic like 510 + 7p + 0.004p^2, So when asked for input, enter the constants(510 7 0.004) in the gui for three plants. Then
+%enter fuel cost/hr for each plant. Total power constraint will be asked later. The algorithm will automatically optimize.
+
 clc 
 clear all
 
@@ -8,19 +13,19 @@ syms p3
 power = 0;
 syms lambda
 
-%------------------ Input for first differential equation ---------------%
+%------------------ Input for first quadratic equation ---------------%
 equation =  inputdlg('Enter the three constants space seperated for power plant equation:',...
              'Power Plant 1', [1 50]);
 equation_constant  =  str2num(equation{1});    
 power_plant1 = equation_constant(1) + equation_constant(2)*p1 + equation_constant(3)*(p1.^2);
 
-%------------------- Input for second differential equation --------------%
+%------------------- Input for second quadratic equation --------------%
 equation =  inputdlg('Enter the three constants space seperated for power plant equation:',...
              'Power Plant 2', [1 50]);
 equation_constant  =  str2num(equation{1}); 
 power_plant2 = equation_constant(1) + equation_constant(2)*p2 + equation_constant(3)*(p2.^2);
 
-% ------------------ Input for third differential equation ---------------%
+% ------------------ Input for third quadratic equation ---------------%
 equation =  inputdlg('Enter the three constants space seperated for power plant equation:',...
              'Power Plant 3', [1 50]);
 equation_constant  =  str2num(equation{1});    
@@ -37,11 +42,11 @@ total_power= input('Enter Total power:');
 power = p1 + p2 +p3 == total_power;
 
 
-total_fuel1 = fuel_cost1*power_plant1;
+total_fuel1 = fuel_cost1*power_plant1; %total fuel is calculted by multiplying it by power plant equation
 total_fuel2 = fuel_cost2*power_plant2;
 total_fuel3 = fuel_cost3*power_plant3;
 
-differential_1 = diff(total_fuel1,p1) == lambda;
+differential_1 = diff(total_fuel1,p1) == lambda; %derivative
 differential_2 = diff(total_fuel2,p2) == lambda;
 differential_3 = diff(total_fuel3,p3) == lambda;
 
